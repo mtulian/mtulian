27b69a2ceebff3b83443e19a8ee0870df824ab8d
Maximiliano Tulian <m.tulian@globant.com>
2019-03-26 01:17:06 -0300

    add husty, react-router, and source-map-explorer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-         Hello world
-        </header>
+        <header className="App-header">Hello world</header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
